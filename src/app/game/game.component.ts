700ef23e2197c74719d493b28a52408e06cce0d4
plantPadel <patrick.wethli@plantup.io>
2025-03-15 20:17:36 +0100

    card animation

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+
+  game: Game;
+  takeCardAnimation = false;
+  currentCard: string | undefined = '';
+
+  constructor() {
+    this.game = new Game();
+  }
+  takeCard() {
+    if (!this.takeCardAnimation) { 
+      this.currentCard = this.game.stack.pop();
+      this.takeCardAnimation = true;
+      setTimeout(()=>{
+        this.takeCardAnimation = false;
+        if (this.currentCard) {
+          this.game.playedCards.push(this.currentCard);
+        }
+      },1500)
+    }
+  }
+
+
 
 }
